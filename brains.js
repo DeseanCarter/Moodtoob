5cbee9d5e2a43e1020d3d0e1d547e2bbdcc24fcf
Bryan Shay <forgetfulmind@gmail.com>
2021-01-08 18:29:26 -0500

    fixed linkage between picture form and mood analysis ajax call

--- brains.js
+++ brains.js
@@ -1,6 +1,7 @@
 
 let currentMood;
-
+let imageSubmit;
+let imageBase64;
 
 function getMood (){
 
@@ -9,16 +10,16 @@ function getMood (){
 var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect"
 
 
-
 $.ajax({
     url: queryURL,
     type: 'POST',
     data: {
-        api_key: api_key,
-        api_secret: api_secret,
+        api_key: "ieZtXQdAZ6VHqF9Eo7YGfpzMqdrsKxuq",
+        api_secret: "qlqHSezGLOP2ls3wFiIeJuULjom0EKYJ",
         return_attributes: "emotion",
-        image_url: "https://i2.wp.com/www.irishcatholic.com/wp-content/uploads/2016/01/child-in-fear.jpg?fit=700%2C400&ssl=1",
+        image_base64: imageBase64,
     },
+ 
 
 
 })
@@ -37,34 +38,41 @@ $.ajax({
     //if you are happiness
     if (happiness >= disgust && happiness >= fear && happiness >= anger && happiness >= neutral && happiness >= sadness && happiness >= surprise){
        //do this
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are Happy!")
     //if you are disgust
     } else if (disgust >= happiness && disgust >= fear && disgust >= anger && disgust >= neutral && disgust >= sadness && disgust >= surprise) {
         //do this
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are disgusted!")
     //if you are fear
     } else if(fear >= happiness && fear >= disgust && fear >= anger && fear >= neutral && fear >= sadness && fear >= surprise) {
         //do this 
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are afraid!")
     }
     //if you are angry 
       else if(anger >= happiness && anger >= disgust && anger >= fear && anger >= neutral && anger >= sadness && anger >= surprise) {
         //do this 
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are angry!")
     }
     //if you are neutral
     else if(neutral >= happiness && neutral >= disgust && neutral >= fear && neutral >= anger && neutral >= sadness && neutral >= surprise) {
         //do this 
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are neutral!")
     }
     //if you are sadness 
     else if(sadness >= happiness && sadness >= disgust && sadness >= fear && sadness >= anger && sadness >= neutral && sadness >= surprise) {
         //do this 
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are sad!")
     } 
     //if you are surprise
     else if(surprise >= happiness && surprise >= disgust && surprise >= fear && surprise >= anger && surprise >= neutral && surprise >= sadness) {
         //do this 
+        $("#moodDisplay").empty();
         $("#moodDisplay").append("You are surprised!")
     } 
 
@@ -82,24 +90,40 @@ $.ajax({
     // });
 
 
-function checkImage(){
-    console.log( "hello!")
-}
+// function checkImage(){
+//     console.log( "hello!")
+//     getMood();
+// }
 
 
 //DESEAN'S AREA 
 
 $(function()
   {
-    $('#testImg').change( function()
+    $('#testImg').change( function(event)
     {
       console.log( $(this).val() );
+      imageSubmit = event.target.files
+      let reader = new FileReader();
+      reader.readAsDataURL(imageSubmit[0]);
+      reader.onload = function () {
+        imageBase64 = reader.result;
+        console.log(reader.result)
+
+    };
     });
   });
 
+  $("#submitBtn").click(function(event){
+      event.preventDefault();
+      console.log("click!");
+      getMood();
+  })
 
-
-
+//   $('#testImg').change( function(event)
+//   {
+//     console.log( event.target.files );
+//   });
 
 
 
---
+++ config.js
@@ -0,0 +1,2 @@
+let api_key = "ieZtXQdAZ6VHqF9Eo7YGfpzMqdrsKxuq"
+let api_secret= "qlqHSezGLOP2ls3wFiIeJuULjom0EKYJ"
